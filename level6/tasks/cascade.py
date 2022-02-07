8d0be8eb7a25ea3948bdd112c553d5e9e88d244d
Kunal <61267953+kunatastic@users.noreply.github.com>
2022-02-07 10:59:49 +0530

    add ulk_update() logic

--- level6/tasks/cascade.py
+++ level6/tasks/cascade.py
@@ -8,14 +8,9 @@ def cascade_logic(user, priority):
     # Array of continuous tasks with increasing priority by 1
     while Task.objects.filter(user=user, priority=priority, completed=False, deleted=False).exists():
         temp_task = Task.objects.filter(user=user, priority=priority, completed=False, deleted=False)[0]
-        print("FOUND THIS",temp_task)
+        temp_task.priority += 1
         store.append(temp_task)
         priority = priority + 1
 
-    # Update the priority of the continuous tasks by 1
-    for task in store:
-        print(task)
-        Task.objects.filter(id=task.id).update(priority=task.priority + 1)
-
     """https://docs.djangoproject.com/en/4.0/ref/models/querysets/#bulk-update"""
-    # Task.objects.update(store, ['priority'])  
+    Task.objects.bulk_update(store, ['priority'])  
